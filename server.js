b084c355614d92e6b425120612d203b107a5f5d1
Ng Zhi An <ngzhian@gmail.com>
2014-07-20 14:37:37 +0800

    Add handler for GET to /messages, simple log

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get("/", function (req, res) {
 	res.sendfile("index.html");
 });
 
+app.get("/messages", function (req, res) {
+	console.log("Got request for all messages");
+});
+
 app.post("/message", function (req, res) {
 	var text = req.body.msgText;
 	console.log("Received message: " + text);
