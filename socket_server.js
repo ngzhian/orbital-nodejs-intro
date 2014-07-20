6d19fe8bcf8dfcf1482b4796675005dc98b103d8
Ng Zhi An <ngzhian@gmail.com>
2014-07-20 14:52:32 +0800

    add socket version of app and server

---
+++ socket_server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+
+app.get("/", function (req, res) {
+	res.sendfile("socket_index.html");
+});
+
+var messages = [];
+
+app.get("/messages", function (req, res) {
+	console.log("Got request for all messages");
+	res.json(messages);
+});
+
+io.sockets.on("connection", function (socket) {
+	socket.on("message", function (data) {
+		console.log("Received: " + data);
+		messages.push(data);
+		io.sockets.send(data);
+	});
+});
+
+server.listen(8000);
+console.log("Listening at port 8000");
