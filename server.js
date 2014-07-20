ae205cbfe97f05fa5983ccf2b83a42541364c205
Ng Zhi An <ngzhian@gmail.com>
2014-07-20 14:16:59 +0800

    Add a post request handler to the app
    Do a simple log to acknowledge the post request and make sure
    our handler is working alright.
    Remember to end the response.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 
+var messages = [];
+
 app.get("/", function (req, res) {
 	console.log("Got a request!");
 	res.sendfile("index.html");
+});
+
+app.post("/message", function (req, res) {
+	console.log("Received message!");
+	res.end();
 });
 
 app.listen(8000);
