c310ef17c54c6cbaaa18b8adb596f46f2fbcc2d8
Ng Zhi An <ngzhian@gmail.com>
2014-07-20 13:28:42 +0800

    Show how we can pass a function as an argument to another function
    This is an example of a callback.
    requestHandler is the callback function passed when server is created.
    Whenever a request is made to the server, the server calls this function.
    The callback has an empty body to illustrate callbacks, we will fill in details
    later.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var http = require('http');
-var server = http.createServer();
+
+var requestHandler = function(request, response) {
+}
+
+var server = http.createServer(requestHandler);
 
 server.listen(8000)
 console.log('Server listening on port 8000');
