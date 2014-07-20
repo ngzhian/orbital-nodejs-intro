21f303cd7db7fec096f5d7dc9bfa35fe5fc18849
Ng Zhi An <ngzhian@gmail.com>
2014-07-20 14:14:15 +0800

    send index.html instead of writing response

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get("*", function (req, res) {
-	res.send("Hello World!");
+app.get("/", function (req, res) {
+	console.log("Got a request!");
+	res.sendfile("index.html");
 });
 
 app.listen(8000);
