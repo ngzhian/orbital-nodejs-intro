658d57bb42305f01190008bd416428db6de3227a
Ng Zhi An <ngzhian@gmail.com>
2014-07-20 13:34:37 +0800

    The handler now writes text as a response
    Take note of `response.end()` it is important to signify the end
    of your response, else the response doesn't get sent.

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var http = require('http');
 
 var requestHandler = function(request, response) {
   console.log('Received request');
+  response.write('Hello World!');
+  response.end();
 }
 
 var server = http.createServer(requestHandler);
 
 server.listen(8000)
 console.log('Server listening on port 8000');
+
+
